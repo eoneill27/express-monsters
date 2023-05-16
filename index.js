cd8ab1947585c29878647ae465edb67282b53e46
Emily <oneill.emily@gmail.com>
2023-05-16 07:30:34 -0700

    Add Express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-index.js
+import express from 'express';
+
+const app = express();
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+app.get('/', function(request, response) {
+	console.log("home page");
+	response.send('hello');
+})
+
+app.use(function(request, response) {
+	response.status(404).send("Sorry, that page doesn't exist.");
+});
+
+// app.listen(1982);
+
+app.listen(1982, function() {
+	console.log("Server started at http://localhost:1982");
+});
