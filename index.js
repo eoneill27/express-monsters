e400dcc8b445e89875695b5fb4eef3a2dc1c91fe
Emily <oneill.emily@gmail.com>
2023-05-16 12:23:09 -0700

    add monster data and style sheets

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import express from 'express';
 
+import monsterData from './monsters.json' assert {type: 'json'};
+
 const app = express();
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.static('css'));
 
 app.get('/', function(request, response) {
 	console.log("home page");
-	response.send('hello');
+	response.render('home');
+})
+
+app.get('/monsters', function(request, response) {
+	response.render('monsters', {monsterArray: monsterData});
+})
+
+app.get('/detail', function(request, response) {
+	response.render('detail');
 })
 
 app.use(function(request, response) {
-	response.status(404).send("Sorry, that page doesn't exist.");
+	response.status(404).render('404', {query: request.url});
 });
 
 // app.listen(1982);
